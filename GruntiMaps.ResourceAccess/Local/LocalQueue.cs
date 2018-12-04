51dbcf164ed245212458524d2e5864017928760f
Chris Green <{ID}+{username}@users.noreply.github.com>
2018-12-04 12:35:07 +1000

    fix local queues so that connections release and don't cause test failures...

--- GruntiMaps.ResourceAccess/Local/LocalQueue.cs
+++ GruntiMaps.ResourceAccess/Local/LocalQueue.cs
@@ -24,20 +24,26 @@ namespace GruntiMaps.ResourceAccess.Local
             var connStr = builder.ConnectionString;
             _queueDatabase = new SqliteConnection(connStr);
             _queueDatabase.Open();
-            const string createQueueTable = "CREATE TABLE IF NOT EXISTS Queue(ID NVARCHAR(50) PRIMARY KEY, PopReceipt NVARCHAR(50) NULL, PopCount INTEGER, Popped NVARCHAR(25) NULL, Content NVARCHAR(2048) NULL)";
-            SqliteCommand createQueueTableCmd = new SqliteCommand(createQueueTable, _queueDatabase);
-            createQueueTableCmd.ExecuteNonQuery();
-            const string createPoisonTable = "CREATE TABLE IF NOT EXISTS Poison(ID INTEGER PRIMARY KEY, PopReceipt NVARCHAR(50) NULL, PopCount INTEGER, Popped NVARCHAR(25) NULL, Content NVARCHAR(2048) NULL)";
-            SqliteCommand createPoisonTableCmd = new SqliteCommand(createPoisonTable, _queueDatabase);
-            createPoisonTableCmd.ExecuteNonQuery();
+            using (SqliteCommand createQueueTableCmd = new SqliteCommand()) {
+                createQueueTableCmd.Connection = _queueDatabase;
+                createQueueTableCmd.CommandText = "CREATE TABLE IF NOT EXISTS Queue(ID NVARCHAR(50) PRIMARY KEY, PopReceipt NVARCHAR(50) NULL, PopCount INTEGER, Popped NVARCHAR(25) NULL, Content NVARCHAR(2048) NULL)";
+                createQueueTableCmd.ExecuteNonQuery();
+            }
+            using (SqliteCommand createPoisonTableCmd = new SqliteCommand()) {
+                createPoisonTableCmd.Connection = _queueDatabase;
+                createPoisonTableCmd.CommandText = "CREATE TABLE IF NOT EXISTS Poison(ID INTEGER PRIMARY KEY, PopReceipt NVARCHAR(50) NULL, PopCount INTEGER, Popped NVARCHAR(25) NULL, Content NVARCHAR(2048) NULL)";
+                createPoisonTableCmd.ExecuteNonQuery();
+            }
         }
 
         public void Clear() // clear the queue of entries - not normally desirable but useful for testing
         {
             System.Diagnostics.Debug.WriteLine($"clearing queue for {_queueDatabase.DataSource} (Enter)");
-            const string delMsg = "DELETE FROM Queue";
-            var delMsgCmd = new SqliteCommand(delMsg, _queueDatabase);
-            delMsgCmd.ExecuteNonQuery();
+            using (var delMsgCmd = new SqliteCommand()) {
+                delMsgCmd.Connection = _queueDatabase;
+                delMsgCmd.CommandText = "DELETE FROM Queue";
+                delMsgCmd.ExecuteNonQuery();
+            }
             System.Diagnostics.Debug.WriteLine($"clearing queue for {_queueDatabase.DataSource} (Exit)");
         }
 
@@ -45,11 +51,13 @@ namespace GruntiMaps.ResourceAccess.Local
         {
             System.Diagnostics.Debug.WriteLine($"adding message to {_queueDatabase.DataSource} (Enter)");
             var id = Guid.NewGuid().ToString();
-            const string addMsg = "INSERT INTO Queue(ID, PopCount, Content) VALUES($QueueId, 0, $content)";
-            var addMsgCmd = new SqliteCommand(addMsg, _queueDatabase);
-            addMsgCmd.Parameters.AddWithValue("$content", message);
-            addMsgCmd.Parameters.AddWithValue("$QueueId", id);
-            addMsgCmd.ExecuteScalar();
+            using (var addMsgCmd = new SqliteCommand()) {
+                addMsgCmd.Connection = _queueDatabase;
+                addMsgCmd.CommandText = "INSERT INTO Queue(ID, PopCount, Content) VALUES($QueueId, 0, $content)";
+                addMsgCmd.Parameters.AddWithValue("$content", message);
+                addMsgCmd.Parameters.AddWithValue("$QueueId", id);
+                addMsgCmd.ExecuteScalar();
+            }
             System.Diagnostics.Debug.WriteLine($"adding message to {_queueDatabase.DataSource} (Exit)");
             return Task.FromResult(id);
         }
@@ -59,34 +67,40 @@ namespace GruntiMaps.ResourceAccess.Local
             System.Diagnostics.Debug.WriteLine($"get message from {_queueDatabase.DataSource} (Enter)");
 
             CheckExpiredMessages();
-            // now we can look for an entry to process.
-            const string getMsg = "SELECT ID, PopCount, Content from Queue WHERE PopReceipt IS NULL LIMIT 1";
-            var getMsgCmd = new SqliteCommand(getMsg, _queueDatabase);
-            var reader = getMsgCmd.ExecuteReader();
-            var popReceipt = Guid.NewGuid().ToString();
-            var pop = reader["PopCount"];
+            Message msg;
             long popCount1;
-            if (DBNull.Value.Equals(pop))
-            {
-                popCount1 = 0;
+            // now we can look for an entry to process.
+            using (var getMsgCmd = new SqliteCommand()) {
+                getMsgCmd.Connection = _queueDatabase;
+                getMsgCmd.CommandText = "SELECT ID, PopCount, Content from Queue WHERE PopReceipt IS NULL LIMIT 1";
+                var reader = getMsgCmd.ExecuteReader();
+                var popReceipt = Guid.NewGuid().ToString();
+                var pop = reader["PopCount"];
+                if (DBNull.Value.Equals(pop))
+                {
+                    popCount1 = 0;
+                }
+                else
+                {
+                    popCount1 = (long)pop;
+                }
+                popCount1++;
+                msg = new Message
+                {
+                    Id = reader["ID"].ToString(),
+                    PopReceipt = popReceipt,
+                    Content = reader["Content"].ToString()
+                };
             }
-            else
-            {
-                popCount1 = (long)pop;
+            
+            using (var updateMsgCmd = new SqliteCommand()) {
+                updateMsgCmd.Connection = _queueDatabase;
+                updateMsgCmd.CommandText = "UPDATE Queue SET PopReceipt = $popReceipt, PopCount = $popCount, Popped = datetime('now') WHERE ID=$ID";
+                updateMsgCmd.Parameters.AddWithValue("$ID", msg.Id);
+                updateMsgCmd.Parameters.AddWithValue("$popReceipt", msg.PopReceipt);
+                updateMsgCmd.Parameters.AddWithValue("$popCount", popCount1);
+                updateMsgCmd.ExecuteReader();
             }
-            popCount1++;
-            var msg = new Message
-            {
-                Id = reader["ID"].ToString(),
-                PopReceipt = popReceipt,
-                Content = reader["Content"].ToString()
-            };
-            const string updateMsg = "UPDATE Queue SET PopReceipt = $popReceipt, PopCount = $popCount, Popped = datetime('now') WHERE ID=$ID";
-            var updateMsgCmd = new SqliteCommand(updateMsg, _queueDatabase);
-            updateMsgCmd.Parameters.AddWithValue("$ID", msg.Id);
-            updateMsgCmd.Parameters.AddWithValue("$popReceipt", msg.PopReceipt);
-            updateMsgCmd.Parameters.AddWithValue("$popCount", popCount1);
-            updateMsgCmd.ExecuteReader();
             System.Diagnostics.Debug.WriteLine($"get message from {_queueDatabase.DataSource} (Exit)");
             return string.IsNullOrWhiteSpace(msg.Id) ? Task.FromResult<Message>(null) : Task.FromResult(msg);
         }
@@ -97,46 +111,54 @@ namespace GruntiMaps.ResourceAccess.Local
 
             // make sure there are no stale messages.
             // If there are we will reset their state (but increment a counter)
-            const string check =
-                "SELECT ID, PopCount from Queue where PopReceipt IS NOT NULL AND Popped < datetime('now', $timeLimit)";
-            var checkCmd = new SqliteCommand(check, _queueDatabase);
-            checkCmd.Parameters.AddWithValue("$timeLimit", $"-{_queueTimeLimit} minutes");
-            var chkReader = checkCmd.ExecuteReader();
-            while (chkReader.Read())
-            {
-                var id = (long)chkReader["ID"];
-                var popCount = (long)chkReader["PopCount"];
-                if (popCount > _queueEntryTries)
-                {   // we exceeded the retry counter so move the entry into the poison table.
-                    const string poison1 = "INSERT INTO Poison(PopReceipt, PopCount, Popped, Content) SELECT PopReceipt, PopCount, Popped, Content FROM Queue WHERE ID=$ID";
-                    var poison1Cmd = new SqliteCommand(poison1, _queueDatabase);
-                    poison1Cmd.Parameters.AddWithValue("$ID", id);
-                    poison1Cmd.ExecuteNonQuery();
-                    const string poison2 = "DELETE FROM Queue WHERE ID=$ID";
-                    var poison2Cmd = new SqliteCommand(poison2, _queueDatabase);
-                    poison2Cmd.Parameters.AddWithValue("$ID", id);
-                    poison2Cmd.ExecuteNonQuery();
-                }
-                else
-                {   // we haven't run out of attempts yet so reset the state but leave counter as-is
-                    const string resetPop = "UPDATE Queue SET PopReceipt = NULL, Popped = NULL WHERE ID=$ID";
-                    var resetPopCmd = new SqliteCommand(resetPop, _queueDatabase);
-                    resetPopCmd.Parameters.AddWithValue("$ID", id);
-                    resetPopCmd.ExecuteNonQuery();
+            using (var checkCmd = new SqliteCommand()) {
+                checkCmd.Connection = _queueDatabase;
+                checkCmd.CommandText = "SELECT ID, PopCount from Queue where PopReceipt IS NOT NULL AND Popped < datetime('now', $timeLimit)";
+                checkCmd.Parameters.AddWithValue("$timeLimit", $"-{_queueTimeLimit} minutes");
+                var chkReader = checkCmd.ExecuteReader();
+                while (chkReader.Read())
+                {
+                    var id = (long)chkReader["ID"];
+                    var popCount = (long)chkReader["PopCount"];
+                    if (popCount > _queueEntryTries)
+                    {   // we exceeded the retry counter so move the entry into the poison table.
+                        using (var poison1Cmd = new SqliteCommand()) {
+                            poison1Cmd.Connection = _queueDatabase;
+                            poison1Cmd.CommandText = "INSERT INTO Poison(PopReceipt, PopCount, Popped, Content) SELECT PopReceipt, PopCount, Popped, Content FROM Queue WHERE ID=$ID";
+                            poison1Cmd.Parameters.AddWithValue("$ID", id);
+                            poison1Cmd.ExecuteNonQuery();
+                        }
+                        using (var poison2Cmd = new SqliteCommand()) {
+                            poison2Cmd.Connection = _queueDatabase;
+                            poison2Cmd.CommandText = "DELETE FROM Queue WHERE ID=$ID";
+                            poison2Cmd.Parameters.AddWithValue("$ID", id);
+                            poison2Cmd.ExecuteNonQuery();
+                        }
+                    }
+                    else
+                    {   // we haven't run out of attempts yet so reset the state but leave counter as-is
+                        using (var resetPopCmd = new SqliteCommand()) {
+                            resetPopCmd.Connection = _queueDatabase;
+                            resetPopCmd.CommandText = "UPDATE Queue SET PopReceipt = NULL, Popped = NULL WHERE ID=$ID";
+                            resetPopCmd.Parameters.AddWithValue("$ID", id);
+                            resetPopCmd.ExecuteNonQuery();
+                        }
+                    }
                 }
             }
             System.Diagnostics.Debug.WriteLine($"expiring messages in queue {_queueDatabase.DataSource} (Exit)");
-
         }
 
         public Task DeleteMessage(Message message)
         {
             System.Diagnostics.Debug.WriteLine($"deleting message from queue {_queueDatabase.DataSource} (Enter)");
-            const string delMsg = "DELETE FROM Queue WHERE ID=$ID AND PopReceipt=$popReceipt";
-            var delMsgCmd = new SqliteCommand(delMsg, _queueDatabase);
-            delMsgCmd.Parameters.AddWithValue("$ID", message.Id);
-            delMsgCmd.Parameters.AddWithValue("$popReceipt", message.PopReceipt);
-            delMsgCmd.ExecuteReader();
+            using (var delMsgCmd = new SqliteCommand()) {
+                delMsgCmd.Connection = _queueDatabase;
+                delMsgCmd.CommandText = "DELETE FROM Queue WHERE ID=$ID AND PopReceipt=$popReceipt";
+                delMsgCmd.Parameters.AddWithValue("$ID", message.Id);
+                delMsgCmd.Parameters.AddWithValue("$popReceipt", message.PopReceipt);
+                delMsgCmd.ExecuteReader();
+            }
             System.Diagnostics.Debug.WriteLine($"deleting message from queue {_queueDatabase.DataSource} (Exit)");
             return Task.CompletedTask;
         }
